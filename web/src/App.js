fbdca22c6b8172f1f913e328c021c6cb800de451
nima-z <nima13zare@gmail.com>
2021-10-15 18:00:13 +0330

    first commit

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Fragment } from "react"
+function App() {
+  return (
+    <Fragment>
+      <div>hello
+      </div>
+      <div>jello</div>
+    </Fragment>
+  );
+}
+
+export default App;

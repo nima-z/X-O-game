1980012740ca57f24093ad67430f81affeb5bc24
nima-z <nima13zare@gmail.com>
2021-11-19 14:43:07 +0330

    add transition to cell

--- web/src/components/Board/Cell.js
+++ web/src/components/Board/Cell.js
@@ -1,14 +1,46 @@
 import React, { Fragment, memo } from "react";
+import { Transition } from "react-transition-group";
+
 import styles from "./Cell.module.css";
+
+const duration = 150;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
 
 function Cell({ player, sound, transportToUp, isTurn, image }) {
   if (sound) sound();
 
   return (
     <Fragment>
-      <td className={styles.cell} onClick={isTurn && !player ? transportToUp : null}>
+      <td
+        className={styles.cell}
+        onClick={isTurn && !player ? transportToUp : null}
+      >
         <div className={styles.holder}>
-          <img src={image} />
+          <Transition in={player} timeout={duration}>
+            {(state) => {
+              console.log(state);
+              return (
+                <img
+                  src={image}
+                  style={{
+                    ...defaultStyle,
+                    ...transitionStyles[state],
+                  }}
+                />
+              );
+            }}
+          </Transition>
         </div>
       </td>
     </Fragment>

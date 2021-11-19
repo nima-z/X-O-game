e5edb3a33a17873b023d92bfa81fea404e98727b
nima-z <nima13zare@gmail.com>
2021-11-19 15:12:21 +0330

    add focus to starting page

--- web/src/components/StartingPage/StartingPage.js
+++ web/src/components/StartingPage/StartingPage.js
@@ -4,18 +4,22 @@ import styles from "./StartingPage.module.css";
 
 function StartingPage(props) {
   const ref = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    props.startAction(ref.current.value);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.input}>
-        <form>
+        <form onSubmit={submitHandler}>
           <label htmlFor="nickName">Nick Name</label>
-          <input type="text" id="nickName" ref={ref} />
+          <input type="text" id="nickName" ref={ref} autoFocus />
+          <div className={styles.action}>
+            <Button type="submit">Play</Button>
+          </div>
         </form>
-      </div>
-      <div className={styles.action}>
-        <Button onClick={() => props.startAction(ref.current.value)}>
-          Play
-        </Button>
       </div>
     </div>
   );
